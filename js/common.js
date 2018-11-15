b39460e0b73d76f454da3ccd37271b261f074e92
Administrator <1105420247@qq.com>
2018-11-15 17:57:09 +0800

    my blog   HTML

---
+++ js/common.js
@@ -0,0 +1,20 @@
+;
+$(document).ready(function () {
+//nav
+//     var obj = null;
+//     var As = document.getElementById('starlist').getElementsByTagName('a');
+//     obj = As[0];
+//     for (i = 1; i < As.length; i++) {
+//         if (window.location.href.indexOf(As[i].href) >= 0)
+//             obj = As[i];
+//     }
+//     obj.id = 'selected';
+
+    //m_nav open close
+    $(".open_nav").click(function () {
+        console.log('111');
+        $(".m_nav_list").toggle();
+        $(".cover").toggle();
+        $(".open_nav").toggleClass("open");
+    });
+});
